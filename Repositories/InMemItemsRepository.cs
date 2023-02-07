445fe5f79a7556c888c04028b1ba4b624d4320f6
Munsanda Mweemba <eliasmweemba498@gmail.com>
2023-02-07 11:07:27 +0300

    Entity, Repository and Controller files added

---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,20 @@
+using Catalog.Entities;
+
+namespace Catalog.Repositories{
+    public class InMemItemsRepository {
+        private readonly List<Item> items = new() {
+            new Item {Id = Guid.NewGuid(), Name = "Portion", Price = 9, CreatedDate = DateTimeOffset.Now},
+            new Item {Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.Now},
+            new Item {Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.Now}
+        };
+
+        public IEnumerable<Item> GetItems(){
+            return items;
+        }
+
+        public Item? GetItem(Guid id){
+            return items.Where(item => item.Id == id)
+                        .SingleOrDefault();
+        }
+    }
+}
